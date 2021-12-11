07926e86fd17a8da1308e9bb1235b69a5ab55ce1
Milan Panta <josephhernandez9841@gmail.com>
2021-12-11 17:46:16 +0545

    feat: connect to the wallet and get details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+
 import BaseLayout from '@utils/layouts/Base';
 import Converter from '@components/Converter';
 
+function getLibrary(provider: any) {
+  return new Web3(provider);
+}
+
 export default function Home() {
   return (
-    <BaseLayout>
-      <Converter />
-    </BaseLayout>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <BaseLayout>
+        <Converter />
+      </BaseLayout>
+    </Web3ReactProvider>
   );
 }
