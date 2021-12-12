9f6afd9e8537bebba87ae2eb0a4db6a4e3617472
Milan Panta <josephhernandez9841@gmail.com>
2021-12-12 14:42:24 +0545

    refactor: clean the code

--- components/PopupModal.tsx
+++ components/PopupModal.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
 
+import { Dialog, Transition } from '@headlessui/react';
 import { Injected } from './Connectors';
 import { useWeb3React } from '@web3-react/core';
 
@@ -87,8 +87,8 @@ export default function PopupModal() {
             </Transition.Child>
 
             <span
-              className="hidden sm:inline-block sm:align-middle sm:h-screen"
               aria-hidden="true"
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
             >
               &#8203;
             </span>
@@ -124,14 +124,14 @@ export default function PopupModal() {
                         <thead>
                           <tr>
                             <th
-                              scope="col"
                               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                              scope="col"
                             >
                               Key
                             </th>
                             <th
-                              scope="col"
                               className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                              scope="col"
                             >
                               Value
                             </th>
@@ -173,20 +173,20 @@ export default function PopupModal() {
                 {!active && (
                   <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button
-                      type="button"
                       className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                       onClick={() => setOpen(false)}
                       ref={cancelButtonRef}
+                      type="button"
                     >
                       Cancel
                     </button>
 
                     <button
-                      type="button"
                       className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm mt-2 sm:mt-0 px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:ml-3 sm:w-auto sm:text-sm"
                       onClick={() => {
                         connect();
                       }}
+                      type="button"
                     >
                       Connect Now
                     </button>
@@ -204,11 +204,11 @@ export default function PopupModal() {
                 {active && (
                   <div className="flex items-center justify-center py-3">
                     <button
-                      type="button"
                       className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 sm:ml-3 sm:w-auto sm:text-sm"
                       onClick={() => {
                         disconnect();
                       }}
+                      type="button"
                     >
                       Disconnect
                     </button>
