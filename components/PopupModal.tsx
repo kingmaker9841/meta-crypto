fa472d341e50dc284071beedf9430e69958750e6
Milan Panta <josephhernandez9841@gmail.com>
2021-12-12 16:25:58 +0545

    feat: handle errors with custom message

--- components/PopupModal.tsx
+++ components/PopupModal.tsx
@@ -3,15 +3,21 @@ import { Fragment, useEffect, useRef, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { formatEther } from '@ethersproject/units';
 import { Injected } from './Connectors';
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from '@web3-react/injected-connector';
 import { Spinner } from './utils/Spinner';
-import { useWeb3React } from '@web3-react/core';
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector';
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector';
+import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
 
 export default function PopupModal() {
   const [open, setOpen] = useState(false);
 
   const cancelButtonRef = useRef(null);
 
-  const { active, account, library, activate, deactivate, chainId } =
+  const { active, account, library, activate, deactivate, chainId, error } =
     useWeb3React();
 
   async function connect() {
@@ -24,6 +30,22 @@ export default function PopupModal() {
 
   async function disconnect() {
     deactivate();
+  }
+
+  function getErrorMessage(error: Error) {
+    if (error instanceof NoEthereumProviderError) {
+      return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.';
+    } else if (error instanceof UnsupportedChainIdError) {
+      return "You're connected to an unsupported network.";
+    } else if (
+      error instanceof UserRejectedRequestErrorInjected ||
+      error instanceof UserRejectedRequestErrorWalletConnect ||
+      error instanceof UserRejectedRequestErrorFrame
+    ) {
+      return 'Please authorize this website to access your Ethereum account.';
+    } else {
+      return 'An unknown error occurred. Check the console for more details.';
+    }
   }
 
   function Account() {
@@ -136,97 +158,108 @@ export default function PopupModal() {
                     </div>
                   </div>
                   <div className="mt-2">
-                    {!active ? (
+                    {!!error ? (
                       <p className="text-sm text-gray-500 sm:pl-4">
-                        Wallet not connected. Please click the &apos;Connect
-                        Now&apos; button below.
+                        {getErrorMessage(error)}
                       </p>
                     ) : (
-                      <table className="w-full divide-y">
-                        <thead>
-                          <tr>
-                            <th
-                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              scope="col"
+                      <>
+                        {!active ? (
+                          <p className="text-sm text-gray-500 sm:pl-4">
+                            Wallet not connected. Please click the &apos;Connect
+                            Now&apos; button below.
+                          </p>
+                        ) : (
+                          <table className="w-full divide-y">
+                            <thead>
+                              <tr>
+                                <th
+                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                  scope="col"
+                                >
+                                  Key
+                                </th>
+                                <th
+                                  className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                  scope="col"
+                                >
+                                  Value
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody className="bg-white">
+                              <tr>
+                                <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
+                                  Account
+                                </td>
+                                <td className="px-6 py-4 text-right text-sm whitespace-nowrap font-medium text-gray-500">
+                                  {account ? (
+                                    <Account />
+                                  ) : (
+                                    <Spinner color="violet" />
+                                  )}
+                                </td>
+                              </tr>
+                              <tr>
+                                <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
+                                  Chain ID
+                                </td>
+                                <td className="px-6 py-4 text-right whitespace-nowrap font-medium text-sm text-gray-500">
+                                  {chainId ? (
+                                    chainId
+                                  ) : (
+                                    <Spinner color="violet" />
+                                  )}
+                                </td>
+                              </tr>
+                              <tr>
+                                <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
+                                  Balance
+                                </td>
+                                <td className="px-6 py-4 text-right flex justify-end items-end whitespace-nowrap font-medium text-sm text-gray-500">
+                                  {active && <Balance />}
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        )}
+
+                        {!active && (
+                          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                            <button
+                              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                              onClick={() => setOpen(false)}
+                              ref={cancelButtonRef}
+                              type="button"
                             >
-                              Key
-                            </th>
-                            <th
-                              className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              scope="col"
+                              Cancel
+                            </button>
+
+                            <button
+                              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm mt-2 sm:mt-0 px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:ml-3 sm:w-auto sm:text-sm"
+                              onClick={() => {
+                                connect();
+                              }}
+                              type="button"
                             >
-                              Value
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody className="bg-white">
-                          <tr>
-                            <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
-                              Account
-                            </td>
-                            <td className="px-6 py-4 text-right text-sm whitespace-nowrap font-medium text-gray-500">
-                              {account ? (
-                                // account?.slice(0, 4) + '..' + account?.slice(-4)
-                                <Account />
-                              ) : (
-                                <Spinner color="violet" />
-                              )}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
-                              Chain ID
-                            </td>
-                            <td className="px-6 py-4 text-right whitespace-nowrap font-medium text-sm text-gray-500">
-                              {chainId ? chainId : <Spinner color="violet" />}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td className="px-6 py-4 whitespace-nowrap font-medium text-sm text-gray-500">
-                              Balance
-                            </td>
-                            <td className="px-6 py-4 text-right flex justify-end items-end whitespace-nowrap font-medium text-sm text-gray-500">
-                              {active && <Balance />}
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
+                              Connect Now
+                            </button>
+                          </div>
+                        )}
+
+                        {active && (
+                          <div className="flex justify-center">
+                            <p className="px-6 py-4 whitespace-nowrap font-small text-sm text-gray-500">
+                              Wallet Details
+                            </p>
+                          </div>
+                        )}
+                      </>
                     )}
                   </div>
                 </div>
 
-                {!active && (
-                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                      type="button"
-                    >
-                      Cancel
-                    </button>
-
-                    <button
-                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm mt-2 sm:mt-0 px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => {
-                        connect();
-                      }}
-                      type="button"
-                    >
-                      Connect Now
-                    </button>
-                  </div>
-                )}
-
-                {active && (
-                  <div className="flex justify-center">
-                    <p className="px-6 py-4 whitespace-nowrap font-small text-sm text-gray-500">
-                      Wallet Details
-                    </p>
-                  </div>
-                )}
-
-                {active && (
+                {(active || error) && (
                   <div className="flex items-center justify-center py-3">
                     <button
                       className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 sm:ml-3 sm:w-auto sm:text-sm"
